7e3090fc0c8d22a6f559f01cd69459e0a7038be9
Sarikaravandale <95906216+Sarikaravandale@users.noreply.github.com>
2022-11-05 11:44:50 +0530

    Add files via upload

---
+++ Basic_Programs/Addition Of Two Numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0,No2 = 0,Sum = 0;
+    
+    printf("\n Enter Frist Number =>");
+    scanf("%d",&No1);
+    
+    printf("\n Enter Second Number =>");
+    scanf("%d",&No2);
+    
+    Sum = No1 + No2;
+    
+    printf("\n Addition Of %d + %d = %d",No1,No2,Sum);
+    
+    
+    getch();
+    return 0;
+    
+    
+ }   
+   
+   
+
