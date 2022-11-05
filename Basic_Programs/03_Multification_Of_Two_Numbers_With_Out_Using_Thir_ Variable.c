901300d701a6138af28dedb7f0e9bafdd87b5676
Sarikaravandale <95906216+Sarikaravandale@users.noreply.github.com>
2022-11-05 12:42:39 +0530

    Basic_Programs

---
+++ Basic_Programs/03_Multification_Of_Two_Numbers_With_Out_Using_Thir_ Variable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0,No2 = 0;
+    
+    printf("\n Enter Frist Number =>");
+    scanf("%d",&No1);
+    
+    printf("\n Enter Second Number =>");
+    scanf("%d",&No2);
+    
+    printf("\n Multification Of %d * %d = %d",No1,No2,No1*No2);
+    
+    getch();
+    return 0;
+    
+    
+}
