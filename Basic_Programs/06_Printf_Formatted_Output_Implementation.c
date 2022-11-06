16fe42b1c2d825bf11d9b0fe2e3c01b73bae1320
Sarikaravandale <95906216+Sarikaravandale@users.noreply.github.com>
2022-11-06 22:44:22 +0530

    Basic_Programs

---
+++ Basic_Programs/06_Printf_Formatted_Output_Implementation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n\t %3d %8s %3d %3d %3d %0.2f",5,"Amit",9,81,150,50.00);
+    
+    printf("\n\t %3d %8s %3d %3d %3d %0.2f",2,"Sujata",70,80,200,66.67);
+    
+    printf("\n\t %3d %8s %3d %3d %3d %0.2f",10,"Ajay",82,18,170,56.67);
+    
+    printf("\n\n Thanks!!!");
+    
+    getch();
+    return 0;
+    
+}   
