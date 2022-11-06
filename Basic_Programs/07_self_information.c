1d97b5572f35c447c3c315c0ec23b4e81c4c559e
Sarikaravandale <95906216+Sarikaravandale@users.noreply.github.com>
2022-11-06 22:46:07 +0530

    Basic_Programs

---
+++ Basic_Programs/07_self_information.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  printf("\n\t Student Information");
+   
+   printf("\n\t Student Name=Sarika Balu Ravandale");
+
+   printf("\n\t Student Roll No=159"); 
+    
+   printf("\n\t Div=B"); 
+     
+   printf("\n\t Age=17");  
+   
+   printf("\n\t Mob No= 7058621922");
+   
+   getch();
+   return 0;
+   
+ }
