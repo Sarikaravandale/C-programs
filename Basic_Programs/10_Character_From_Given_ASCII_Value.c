1c9d583a293057d2a491480b3389a96d4fb7577f
Sarikaravandale <95906216+Sarikaravandale@users.noreply.github.com>
2022-11-07 21:40:35 +0530

    Basic_Programs

---
+++ Basic_Programs/10_Character_From_Given_ASCII_Value.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int No = 0;
+    
+    printf("\n Enter Your Value");
+    scanf("%d",&No);
+    
+    printf("\n Value Of ASCII =%c",No);
+    
+    getch();
+    return 0;
+    
+}  
