2cb462f28b5fe9465bdb539e0677516ba9e3de0c
Sarikaravandale <95906216+Sarikaravandale@users.noreply.github.com>
2022-11-07 21:41:13 +0530

    Basic_Programs

---
+++ Basic_Programs/11_ASCII_Value_Of_Given_Character.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+ 
+ int main()
+ {
+   
+     char ch = '\0';
+     
+     printf("\n\t Enter A Character =>");
+     scanf("%c",&ch);
+     
+     printf("\n ASCII Value Of Given Character %c = %d",ch,ch);
+     
+     getch();
+     return 0;
+     
+ }    
