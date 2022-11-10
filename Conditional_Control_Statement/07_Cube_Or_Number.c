2d6f1563523c1a38c0d0357bb1d0145fcde5485b
Sarikaravandale <95906216+Sarikaravandale@users.noreply.github.com>
2022-11-10 23:22:49 +0530

    Conditional_Programs

---
+++ Conditional_Control_Statement/07_Cube_Or_Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 0,Cube = 0;
+    
+    printf("\n Enter The Number =>");
+    scanf("%d",&Num);
+    
+    Cube = Num * Num * Num;
+    
+    printf("\n Number Of Cube Is %d = %d",Num,Cube);
+    
+    printf("\n Thanks!!!");
+    
+    getch();
+    return 0;
+    
+}    
+   
+    
+    
