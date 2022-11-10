bddd7369fac2f23d4550d38bcc2cc6505f4baa30
Sarikaravandale <95906216+Sarikaravandale@users.noreply.github.com>
2022-11-10 23:21:35 +0530

    Conditional_Programs

---
+++ Conditional_Control_Statement/06_Sqare_Of_Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 0,Sqare = 0;
+    
+    printf("\n Enter Your Value =>");
+    scanf("%d",&Num);
+    
+    Sqare = Num * Num;
+    
+    printf("\n  Number Of Given Sqare Of %d = %d",Num,Sqare);
+    
+    printf("\n Thanks!!!");
+    
+    getch();
+    return 0;
+}        
